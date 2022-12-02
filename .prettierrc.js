747a97b9082877245069b91e303a54c2e9b05869
Aprajati Siuli <aprajati.siuli@brainiuminfotech.com>
2022-12-02 18:50:05 +0530

    module path

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+};
